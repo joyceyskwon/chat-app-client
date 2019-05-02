8b9e4828cff6e48c371b2acb4a62f7cbb283f7fe
joyceyskwon <joyceyskwon@gmail.com>
2019-05-01 23:40:41 -0400

    trying to fix delete function

--- src/components/TweetList.js
+++ src/components/TweetList.js
@@ -1,26 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Tweet from './Tweet'
 import NewTweetInfo from './NewTweetInfo'
 import adapter from '../services/adapter'
 
-const deleteTweet = tweetId => adapter.deleteTweet(tweetId)
-
-const TweetList = ({ tweets, newTweetCount, handleDisplayTweets }) => {
-
+class TweetList extends Component {
+  
+  deleteTweet = tweetId => adapter.deleteTweet(tweetId)
+  
   // tweets are reversed props from Feed.js
-  const tweetList = tweets.map(tweet => <Tweet key={tweet.id} tweet={tweet} deleteTweet={deleteTweet} />)
-
-  return (
-    <div>
-      <NewTweetInfo
-        handleDisplayTweets={handleDisplayTweets}
-        newTweetCount={newTweetCount}
-      />
-      <div className="ui feed">
-        {tweetList}
+  const tweetList = tweets => tweets.map(tweet => <Tweet key={tweet.id} tweet={tweet} deleteTweet={this.deleteTweet} />)
+  
+  render() {
+    return (
+      <div>
+        <NewTweetInfo
+          handleDisplayTweets={handleDisplayTweets}
+          newTweetCount={newTweetCount}
+        />
+        <div className="ui feed">
+          {tweetList}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default TweetList
