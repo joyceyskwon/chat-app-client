c64329d8ee6c81dd353fac957615f642821668a6
joyceyskwon <joyceyskwon@gmail.com>
2019-05-21 16:00:22 -0400

    removed api ws url

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import { ActionCableProvider } from 'react-actioncable-provider'
-import adapter from './services/adapter'
 import App from './components/App';
 
-
 ReactDOM.render(
-  <ActionCableProvider url={adapter.API_WS_ROOT}>
+  <ActionCableProvider url='ws://localhost:3000/cable'>
     <App />
   </ActionCableProvider>,
   document.getElementById('root')
